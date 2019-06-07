8054f189780bc12bf144d35c1023dff96c3d5320
Utilisateur <franck.ledoux.pro@gmail.com>
2019-06-07 20:14:12 +0200

    javascript example that load data when component is mounted

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { Post } from './Post';
+import { Posts } from './Post';
+import styled, { keyframes } from 'styled-components';
+
+const Header = styled.header`
+  text-align: center;
+  background-color: #282c34;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Rotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}`;
+
+const Img = styled.img`
+  animation: infinite 20s linear;
+  animation-name: ${Rotation};
+  height: 40vmin;
+  pointer-events: none;
+`
+
+const Container = styled.div`
+  margin: 14px;
+`;
 
 function App() {
-
-  // const config = useMemo(
-  //   () => ({ method: 'get', baseURL: 'https://jsonplaceholder.typicode.com/posts/1' }),
-  //   []
-  // );
-  // const [post2] = useAxios(config);
   return (
     <>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Post />
+      <Header>
+        <Img src={logo} alt="logo" />
+      </Header>
+      <Container>
+        <Posts />
+      </Container>
     </>
   );
 }
--- javascript/src/Post.jsx
+++ javascript/src/Post.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useFetch } from 'react-use-query';
 import { Card } from 'semantic-ui-react'
 
-export const Post = () => {
-  const [{ state, response }] = useFetch('https://jsonplaceholder.typicode.com/posts/1');
+export const Posts = () => {
+  const [{ state, response }] = useFetch('https://jsonplaceholder.typicode.com/posts');
   switch (state) {
     case 'loading':
       return 'Please wait while loading';
     case 'loaded':
       return (
-        <Card>
-          <Card.Content>
-            <Card.Header>{response.title}</Card.Header>
-            <Card.Description>{response.body}</Card.Description>
-          </Card.Content>
-        </Card>
-      );
+        <Card.Group>
+          {response.map(post => <Post key={post.id} {...post} />)}
+        </Card.Group>);
     default:
-      return 'Sorry, sheets happen';
+      return 'Sorry, shits happen';
   }
+}
 
+const extractUserName = (state, response) => {
+  switch (state) {
+    case 'loaded':
+      return response.username;
+    case 'loading':
+    case 'nothing':
+      return 'Loading...';
+    default:
+      return '';
+  }
+};
+
+export const Post = post => {
+  const [userName, setUserName] = useState('Loading...');
+  const [{ state, response }] = useFetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`);
+  useEffect(
+    () => setUserName(extractUserName(state, response)),
+    [state, response]
+  );
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{post.title}</Card.Header>
+        <Card.Description>{post.body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        Author: {userName}
+      </Card.Content>
+    </Card>
+  );
 }
