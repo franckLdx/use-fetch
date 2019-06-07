f8d014b6a0466dd826e3fce304d9cf1aeb8cf352
Utilisateur <franck.ledoux.pro@gmail.com>
2019-06-07 20:21:02 +0200

    refacto

--- react-use-query/README.md
+++ react-use-query/README.md
@@ -1,2 +1,21 @@
 # useQuery
 A react hook that encapsulates boilerpart that comes when doing query into functional components 
+
+# Usage:
+To execute a request when component is mounted:
+~~~javascript
+export const Posts = () => {
+  const [{ state, response }] = useFetch('https://jsonplaceholder.typicode.com/posts');
+  switch (state) {
+    case 'loading':
+      return 'Please wait while loading';
+    case 'loaded':
+      return (
+        <Card.Group>
+          {response.map(post => <Post key={post.id} {...post} />)}
+        </Card.Group>);
+    default:
+      return 'Sorry, shits happen';
+  }
+}
+~~~
--- react-use-query/src/useFetch.ts
+++ react-use-query/src/useFetch.ts
@@ -1,5 +1,5 @@
 import { useCallback, useReducer, useEffect, useMemo } from 'react';
-import { reducer } from './common';
+import { reducer } from './utils';
 
 export function useFetch(initialQuery: string) {
   const [state, dispatch] = useReducer(
@@ -12,6 +12,11 @@ export function useFetch(initialQuery: string) {
       try {
         dispatch({ type: 'LOADING' });
         const raw = await fetch(query);
+        if (!raw.ok) {
+          console.log(raw);
+          dispatch({ type: 'ERROR', error: raw });
+          return;
+        }
         const response = await raw.json();
         dispatch({ type: 'LOADED', response });
       } catch (error) {
