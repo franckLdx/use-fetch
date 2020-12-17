b7ce00434d2fbf6c35e68461507acbb207369037
Utilisateur <franck.ledoux.pro@gmail.com>
2020-12-17 18:16:12 +0100

    bump deps

--- hook-use-fetch/lib/useFetch.js
+++ hook-use-fetch/lib/useFetch.js
@@ -1,8 +1,9 @@
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -13,7 +14,7 @@ export function useFetch(query, dependencies) {
     useEffect(() => { executeQuery(query, dispatch); }, dependencies);
     return [state.fetchState, state.result];
 }
-const executeQuery = (query, dispatch) => __awaiter(this, void 0, void 0, function* () {
+const executeQuery = (query, dispatch) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         dispatch({ type: 'LOADING' });
         if (!query) {
